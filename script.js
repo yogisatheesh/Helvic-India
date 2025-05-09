9ba3ac8135a5f0ef4ac45fe3c9f3670376c726f6
yogisatheesh <selva.yogi1@gmail.com>
2025-05-09 23:42:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Toggle navbar for mobile screens
+document.getElementById("menu-toggle").addEventListener("click", function() {
+  var navLinks = document.querySelector(".nav-links");
+  navLinks.classList.toggle("show");
+});
